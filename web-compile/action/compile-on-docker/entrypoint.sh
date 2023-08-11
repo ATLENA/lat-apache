ca1074f26ba2c68b6f9678db94d1f8bc464fe6a0
SONGYEONWON <songyeon.won@lgcns.com>
2023-08-11 15:05:53 +0900

    feat: support_multi_os

--- web-compile/action/compile-on-docker/entrypoint.sh
+++ web-compile/action/compile-on-docker/entrypoint.sh
@@ -8,10 +8,8 @@ chmod -R 755 ./web-compile
 
 # build apache httpd
 if [[ $TARGET_OS == *"ubuntu"* ]]; then
-  echo "===== ubuntu"
   ./web-compile/bin/web-build-ubuntu.sh
 else
-  echo "===== not ubuntu"
   ./web-compile/bin/web-build.sh
 fi
 
